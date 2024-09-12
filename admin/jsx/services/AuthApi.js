092697f2030b1af56c9acfbf5fedd6c0fb4b4422
iniznet <iniznet@gmail.com>
2024-09-12 15:34:55 +0700

    remove jsx converted

--- admin/jsx/services/AuthApi.js
+++
@@ -1,9 +0,0 @@
-import Api from './Api';
-const AuthApi = {
-    login: (credentials) => Api.post('/login', credentials),
-    register: (credentials) => Api.post('/register', credentials),
-    logout: () => Api.post('/logout'),
-    refresh: () => Api.post('/refresh'),
-    getCurrentUser: () => Api.get('/user'),
-};
-export default AuthApi;
